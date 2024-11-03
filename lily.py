84f9a851f1c4fcc1722678ed1fae254efc834349
idk-aarza <aarzak2012@gmail.com>
2024-11-03 10:42:10 +0530

    lily

---
+++ lily.py
@@ -0,0 +1,6 @@
+string1=input("Enter any string word")
+string2=('')
+for i in string1:
+    string2=i+string2
+print("The original string is", string1)
+print('The reversed string is', string2)
---
+++ rose.py
@@ -0,0 +1,6 @@
+k=int(input('Enter a number'))
+sum=0
+for i in range(1, k+1):
+    sum+=i
+    print(sum)
+#if k=5 then we will write 1+2+3+4+5 then it will be 1, 3, 6, 10
